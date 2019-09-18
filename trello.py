5f324893d4718cbbe00ef73696f7e77ef80355d4
unknown <my@email.com>
2019-09-18 11:27:15 -0700

    init

---
+++ trello.py
@@ -0,0 +1,87 @@
+import sys
+import requests
+
+base_url = "https://api.trello.com/1/{}"
+
+auth_params = {
+    'key': "e3af0fe8e062b2b6ca5f9c906b8dc7fd",
+    'token': "08100766b52d1e38c68c2e6002abe277062726959a3126dae5a5dbb579ed2d3a",
+}
+
+board_id = "y4ohsuNE"
+
+'''
+   -1     0        1    2        3
+python trello.py read
+
+python trello.py create "Изучить Python" "Нужно сделать"
+# Добавить создание колонки
+
+python trello.py move "Изучить Python" "В процессе"
+'''
+
+# TODO Сделать код объектно ориентированным, убрать повторяемый код
+
+
+def read():
+    # Получим данные всех колонок на доске:
+    column_data = requests.get(base_url.format('boards') + '/' + board_id + '/lists', params=auth_params).json()
+
+    # Теперь выведем название каждой колонки и всех заданий, которые к ней относятся:
+    for column in column_data:
+        print(column['name'])
+        # Получим данные всех задач в колонке и перечислим все названия
+        task_data = requests.get(base_url.format('lists') + '/' + column['id'] + '/cards', params=auth_params).json()
+        if not task_data:
+            print('\t' + 'Нет задач!')
+            continue
+        for task in task_data:
+            print('\t' + task['name'])
+
+
+def create(name, column_name):
+    # Получим данные всех колонок на доске
+    column_data = requests.get(base_url.format('boards') + '/' + board_id + '/lists', params=auth_params).json()
+    # TODO: Сделать создание колонки, если такое имя не существует
+
+    # Переберём данные обо всех колонках, пока не найдём ту колонку, которая нам нужна
+    for column in column_data:
+        if column['name'] == column_name:
+            # Создадим задачу с именем _name_ в найденной колонке
+            requests.post(base_url.format('cards'), data={'name': name, 'idList': column['id'], **auth_params})
+            break
+
+
+def move(name, column_name):
+    # Получим данные всех колонок на доске
+    column_data = requests.get(base_url.format('boards') + '/' + board_id + '/lists', params=auth_params).json()
+
+    # TODO: Можете ли вы предсказать, какие проблемы могут возникнуть, если мы назовём две задачи одинаково?
+    # Среди всех колонок нужно найти задачу по имени и получить её id
+    task_id = None
+    for column in column_data:
+        column_tasks = requests.get(base_url.format('lists') + '/' + column['id'] + '/cards', params=auth_params).json()
+        for task in column_tasks:
+            if task['name'] == name:
+                task_id = task['id']
+                break
+        if task_id:
+            break
+
+    # Теперь, когда у нас есть id задачи, которую мы хотим переместить
+    # Переберём данные обо всех колонках, пока не найдём ту, в которую мы будем перемещать задачу
+    for column in column_data:
+        if column['name'] == column_name:
+            # И выполним запрос к API для перемещения задачи в нужную колонку
+            requests.put(base_url.format('cards') + '/' + task_id + '/idList', data={'value': column['id'],  **auth_params})
+            break
+
+
+if __name__ == "__main__":
+    if len(sys.argv) <= 2:
+        read()
+    elif sys.argv[1] == 'create':
+        create(sys.argv[2], sys.argv[3])
+    elif sys.argv[1] == 'move':
+        move(sys.argv[2], sys.argv[3])
+
